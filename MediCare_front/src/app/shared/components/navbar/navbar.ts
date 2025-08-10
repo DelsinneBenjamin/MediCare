ddd4a34f5875f8f94dd1b3eae5a95cce00517810
D.Benjamin <68815764+SkyDozZ@users.noreply.github.com>
2025-08-10 22:02:52 +0200

    Update gitignore for cache Angular

--- MediCare_front/src/app/shared/components/navbar/navbar.ts
+++ MediCare_front/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+    isOpen = false;
 
+    toggleOpen(){
+      this.isOpen = !this.isOpen;
+    }
 }
