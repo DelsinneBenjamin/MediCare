36d9103125f44f47b5634f147fbd46bef1f0066b
D.Benjamin <68815764+SkyDozZ@users.noreply.github.com>
2025-09-08 21:59:03 +0200

    push sauvegarde

--- MediCare_back/CustomUser/models.py
+++ MediCare_back/CustomUser/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from location.models import Address, Site
+
+
 # Create your models here.
 class CustomUser(AbstractUser):
     ROLES_CHOICES = [
@@ -10,6 +13,10 @@ class CustomUser(AbstractUser):
 
     role = models.CharField(max_length=20, choices=ROLES_CHOICES)
 
+    #Un user à 1 seule adresse, une adresse peu avoir plusieurs user(genre une famille)
+    #Many To One Adresse 1,N - 1,1 User
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="customuser")
+
 class Doctor(models.Model):
     #Ici un Docteur EST un user
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='doctor')
@@ -18,9 +25,21 @@ class Doctor(models.Model):
     speciality = models.CharField(max_length=20)
     inami_number = models.CharField(max_length=20)
 
+<<<<<<< Updated upstream
     #Relation ManyToMany avec Patient : Un docteur peut avoir plusieurs patients et un patient peut avoir plusieurs docteurs
     patients = models.ManyToManyField('Patient', related_name='doctors', blank=True)
 
+=======
+<<<<<<< Updated upstream
+=======
+    #Relation ManyToMany avec Patient : Un docteur peut avoir plusieurs patients et un patient peut avoir plusieurs docteurs
+    patients = models.ManyToManyField('Patient', related_name='doctor', blank=True)
+
+    # Un médecin peut travailler dans plusieurs sites
+    sites = models.ManyToManyField(Site, related_name="doctor")
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     def __str__(self):
         return f"Dr. {self.user.get_full_name()} - {self.speciality} ({self.inami_number})"
 
---
+++ MediCare_back/location/models.py
@@ -0,0 +1,23 @@
+from tkinter.constants import CASCADE
+
+from django.db import models
+
+# Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    number = models.CharField(max_length=10, blank=True, null=True)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, default="Belgium")
+
+
+
+class Site(models.Model):
+    name = models.TextField()
+    site_type = models.TextField(
+        choices= [("HOSPITAL", "Hôpital"),
+                  ("CLINIC", "Clinique"),
+                  ("CABINET", "Cabinet")]
+    )
+
+    address = models.ManyToManyField(Address, related_name="site")
