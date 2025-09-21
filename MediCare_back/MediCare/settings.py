9f7eb3f5be743f2323e4f87613ebe942e318ba21
D.Benjamin <68815764+SkyDozZ@users.noreply.github.com>
2025-09-21 21:25:57 +0200

    add swagger

--- MediCare_back/MediCare/settings.py
+++ MediCare_back/MediCare/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'appointment',
     'location',
     'rest_framework',
+    'drf_yasg',
     'rest_framework_simplejwt',
     'corsheaders',
 ]
--- MediCare_back/MediCare/urls.py
+++ MediCare_back/MediCare/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from CustomUser.views import UserViewSet, DoctorViewSet, PatientViewSet
 from ordonnance.views import OrdonnanceViewSet
@@ -28,11 +34,19 @@ router.register('doctors', DoctorViewSet, basename='doctor')
 router.register('patients', PatientViewSet, basename='patient')
 router.register('ordonnances', OrdonnanceViewSet, basename='ordonnance')
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Medicare",
+        default_version='v0.0.1',
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
 
-    # JWT Auth
+path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #JWT Auth
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
